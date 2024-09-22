dc72cd1f5f440dde2cb6530e1461f7df9989ab02
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-22 20:18:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function volume_sphere() {
     //Write your code here
-  
+let Radius = document.getElementById('radius');
+
+let rvalue = parseFloat(radius.value);
+
+	if (isNaN(rvalue) || rvalue<0) {
+		document.getElementById('volume').value = 'NaN';
+	}
+	else{
+		let volume = 4/3 * Math.PI * Math.pow(rvalue , 3);
+		volume = volume.tofixed(4);
+		document.getElementById('volume').value = volume;
+	}
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
